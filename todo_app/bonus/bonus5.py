b5e2dc2079333a95b38b4464bfc60d479474dcb4
Arin <arin.apichitchon@gmail.com>
2025-06-12 08:47:45 -0400

    bonus5

--- todo_app/bonus/bonus5.py
+++ todo_app/bonus/bonus5.py
@@ -1,1 +1,6 @@
-print(f"Test push")
+waiting_list = ['sen', 'ben', 'john']
+waiting_list.sort() # list are mutable so it doesn't return anything
+# waiting_list.sort(reverse=True)
+for idx, item in enumerate(waiting_list):
+    row = f"{idx + 1}. {item.capitalize()}"
+    print(row)
