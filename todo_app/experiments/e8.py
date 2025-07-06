18fe714db7a1da6bdb9229354f169daf19bbaa42
Arin <arin.apichitchon@gmail.com>
2025-07-06 14:47:43 -0400

    e8

---
+++ todo_app/experiments/e8.py
@@ -0,0 +1,29 @@
+a = 5
+b = 10
+if a < 10 and b > 5:
+    print("Both conditions are True")  # This will print
+else:
+    print("At least one condition is False")
+
+
+a = 5
+b = 2
+if a > 10 or b < 5:
+    print("At least one condition is True")  # This will print
+else:
+    print("Both conditions are False")
+
+is_raining = False
+if not is_raining:
+    print("You don't need an umbrella!")  # This will print
+else:
+    print("Take an umbrella.")
+
+a = 7
+if not (a > 10):
+    print("a is not greater than 10")  # This will print
+else:
+    print("a is greater than 10")
+
+
+
