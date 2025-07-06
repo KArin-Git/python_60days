5923a7015f8e39a5239cd7557c38445ec6658a38
Arin <arin.apichitchon@gmail.com>
2025-07-06 14:36:00 -0400

    opt

--- todo_app/main.py
+++ todo_app/main.py
@@ -8,22 +8,22 @@ while True:
         todos.append(todo)
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
-    if 'show' in user_action:
+    elif 'show' in user_action:
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
         for idx, item in enumerate(todos):
             item = item.strip('\n')
             print(f"{idx + 1} - {item}")
-    if 'edit' in user_action:
-        idx = int(input("Number of the todo to edit: "))
+    elif 'edit' in user_action:
+        idx = int(user_action[5:])
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
         edit_todo = input("Enter new todo: ")
         todos[idx - 1] = edit_todo + '\n'
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
-    if 'complete' in user_action:
-        idx = int(input("Number of the todo to complete: "))
+    elif 'complete' in user_action:
+        idx = int(user_action[9:])
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
         completed_todo = todos.pop(idx - 1)
@@ -31,7 +31,10 @@ while True:
         print(f"{completed_todo} is completed")
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
-    if 'exit' in user_action:
+    elif 'exit' in user_action:
         break
+
+    else:
+        print("Command is not valid")
 
 print("Bye!")
