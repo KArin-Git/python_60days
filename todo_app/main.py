6bd10f62efd526b2cb274bd71144b997d689cbf7
Arin <arin.apichitchon@gmail.com>
2025-07-07 20:05:11 -0400

    bug fixed

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,20 +1,20 @@
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
-    if 'add' in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:]
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
-        todos.append(todo)
+        todos.append(todo + '\n')
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
-    elif 'show' in user_action:
+    elif user_action.startswith("show"):
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
         for idx, item in enumerate(todos):
             item = item.strip('\n')
             print(f"{idx + 1} - {item}")
-    elif 'edit' in user_action:
+    elif user_action.startswith("edit"):
         idx = int(user_action[5:])
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
@@ -22,7 +22,7 @@ while True:
         todos[idx - 1] = edit_todo + '\n'
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
-    elif 'complete' in user_action:
+    elif user_action.startswith("complete"):
         idx = int(user_action[9:])
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
@@ -31,7 +31,7 @@ while True:
         print(f"{completed_todo} is completed")
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
-    elif 'exit' in user_action:
+    elif user_action.startswith("exit"):
         break
 
     else:
