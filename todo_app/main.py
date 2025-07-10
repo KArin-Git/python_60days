e9a9bd90b22e00d14bffd32482f2f7abc10c99cd
Arin <arin.apichitchon@gmail.com>
2025-07-10 08:58:44 -0400

    write_todos()

--- todo_app/main.py
+++ todo_app/main.py
@@ -3,7 +3,11 @@ def get_todos(filepath='files/todos.txt'):
         todos_local = file_local.readlines()
     return todos_local
 
+def write_todos(todos_arg, filepath='files/todos.txt'):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
 
+# main
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
@@ -11,8 +15,7 @@ while True:
         todo = user_action[4:]
         todos = get_todos()
         todos.append(todo + '\n')
-        with open('files/todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos(todos)
     elif user_action.startswith("show"):
         todos = get_todos()
         for idx, item in enumerate(todos):
@@ -24,8 +27,7 @@ while True:
             todos = get_todos()
             edit_todo = input("Enter new todo: ")
             todos[idx - 1] = edit_todo + '\n'
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid. Please try again.")
             continue
@@ -36,8 +38,7 @@ while True:
             completed_todo = todos.pop(idx - 1)
             completed_todo = completed_todo.strip('\n')
             print(f"{completed_todo} is completed")
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(todos)
         except IndexError:
             print("There is no item with that number. Please try again.")
             continue
