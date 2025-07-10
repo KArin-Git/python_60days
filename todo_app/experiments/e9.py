046867ae036d4a4eebb176d59fa7acd4bb750634
Arin <arin.apichitchon@gmail.com>
2025-07-10 08:46:38 -0400

    sending parameter to func()

--- todo_app/experiments/e9.py
+++ todo_app/experiments/e9.py
@@ -8,8 +8,13 @@ def greet2():
     msg = "Hi"
     new_msg = msg.capitalize()
 
+def greet3(msg):
+    new_msg = msg.capitalize()
+    return new_msg
+
 
 greeting = greet()
 print(greeting) # print new_msg which is Hello
 print(greet2()) # print None
 # print(len(greet2())) # TypeError
+print(greet3("this is greet 3"))
