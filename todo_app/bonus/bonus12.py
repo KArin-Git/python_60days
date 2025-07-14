c3162bbcc7feeca253e14fc2505b1139e825e3cb
Arin <arin.apichitchon@gmail.com>
2025-07-13 21:09:08 -0400

    change return statement

--- todo_app/bonus/bonus12.py
+++ todo_app/bonus/bonus12.py
@@ -1,10 +1,15 @@
-def convert(feet_inches):
-    parts = feet_inches.split(" ")
+def convert(feet_inches_arg):
+    parts = feet_inches_arg.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
     meters = (feet * 0.3048) + (inches * 0.0254)
-    return f"{feet} feet and {inches} inches is equal to {meters} meters."
-
+    # return f"{feet} feet and {inches} inches is equal to {meters} meters."
+    return meters
 
 feet_inches = input("Enter feet and inches: ")
-print(convert(feet_inches))
+# print(convert(feet_inches))
+result = convert(feet_inches)
+if result < 1:
+    print("Kid is too small")
+else:
+    print("Kid can use the slide")
