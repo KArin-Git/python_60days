6af18d42b4d24ec747138bf2b6b4c1e1d7809bcf
Arin <arin.apichitchon@gmail.com>
2025-07-15 10:26:36 -0400

    e10

---
+++ todo_app/experiments/e10.py
@@ -0,0 +1,24 @@
+text = """
+Hello!
+This is a message
+that spans across
+multiple lines.
+"""
+
+text2 = "\
+Hello! This is text 2\
+This is a message\
+that spans across\
+multiple lines.\
+"
+
+text3 = "\
+Hello! this is text 3\n\
+This is a message\n\
+that spans across\n\
+multiple lines.\n\
+"
+
+print(text)
+print(text2)
+print(text3)
