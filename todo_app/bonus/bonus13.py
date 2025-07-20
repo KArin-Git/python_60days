577260758e0978c37ec0bfdd1ab41cdd0a25cfed
Arin <arin.apichitchon@gmail.com>
2025-07-20 13:27:44 -0400

    bonus13

---
+++ todo_app/bonus/bonus13.py
@@ -0,0 +1,17 @@
+def parse(feet_inches_arg):
+    parts = feet_inches_arg.split(" ")
+    feet = float(parts[0])
+    inches = float(parts[1])
+    return feet, inches
+
+def convert(feet, inches):
+    meters = (feet * 0.3048) + (inches * 0.0254)
+    return meters
+
+feet_inches = input("Enter feet and inches: ")
+f, i = parse(feet_inches)
+result = convert(f, i)
+if result < 1:
+    print("Kid is too small")
+else:
+    print("Kid can use the slide")
