6451819db38b9f5a834f3413d36838e265a26b00
Arin <arin.apichitchon@gmail.com>
2025-08-07 08:50:34 -0400

    standalone exe with platyus

--- todo_app/gui.py
+++ todo_app/gui.py
@@ -1,6 +1,11 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists('todo.txt'):
+    with open('todo.txt', 'w') as file:
+        pass
 
 sg.theme('Black')
 clock = sg.Text('', key='clock')
